977dfef0237049d0a6036cd82a27b5c9dfef4161
mohsinshaikh10 <78855163+mohsinshaikh10@users.noreply.github.com>
2021-03-06 11:14:10 +0530

    faceRec mark1
    version 1 of the face detection and recognition module.

---
+++ tester.py
@@ -0,0 +1,38 @@
+import cv2
+import os
+import numpy as np
+import faceRecogntion as fr
+
+test_img=cv2.imread("./testImg/test4.jpg")
+faces_detected,gray_img=fr.faceDetection(test_img)
+
+#for (x,y,w,h) in faces_detected:
+#    cv2.rectangle(test_img,(x,y),(x+w,y+h),(0,255,0),thickness=2)
+    
+#resized_img=cv2.resize(test_img,(1000,700))
+#cv2.imshow("title",resized_img)
+#cv2.waitKey(0)
+#cv2.destroyAllWindows
+
+faces,faceID,=fr.labels_for_training_data('./trainingImg/')
+face_recognizer=fr.train_classifier(faces,faceID)
+face_recognizer.save('./trainingData.xml')
+name={0:"Shahruk", 1:"Priyanka"}
+
+for face in faces_detected:
+    (x,y,w,h)=face
+    roi_gray=gray_img[y:y+h,x:x+w]
+    label,confidence=face_recognizer.predict(roi_gray)
+    print("confidence: ",confidence)
+    print("label: ",label)
+    if(confidence<=41):
+        fr.draw_rect(test_img,face)
+    else:
+        continue
+    predicted_name=name[label]
+    fr.put_text(test_img,predicted_name,x,y)
+    
+resized_img=cv2.resize(test_img,(1000,700))
+cv2.imshow("title",resized_img)
+cv2.waitKey(0)
+cv2.destroyAllWindows
